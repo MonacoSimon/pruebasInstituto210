151f0af520480aa8b591a2851fabd52c24f739c2
leonelloloy <agutvor@gmail.com>
2023-11-03 20:22:44 -0300

    cambio log in

--- cypress/e2e/prueba_login.cy.js
+++ cypress/e2e/prueba_login.cy.js
@@ -1,5 +1,9 @@
 describe("", () =>{
     it ("log in", () =>{
         cy.visit("http://127.0.0.1:8000/")
+        cy.get('.icono-nav').click()
+        cy.get('#id_username').type("vale.balma95@gmail.com")
+        cy.get('#id_password').type("riverplate")
+        cy.get('.btn').click()
     })   
 })
