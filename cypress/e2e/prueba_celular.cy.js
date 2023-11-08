024ac0678c22c80bf5587346c3dbb60b57351351
simon <simonmonaco86@gmail.com>
2023-11-08 18:58:09 -0300

    prueba responsivo

--- cypress/e2e/prueba_celular.cy.js
+++ cypress/e2e/prueba_celular.cy.js
@@ -1,8 +1,7 @@
-const { beforeEach } = require("mocha")
 
 describe("suite pruebas celular", () =>{
-    cy.visit("http://127.0.0.1:8000/")
     it("prueba samsung", () =>{
+        cy.visit("http://127.0.0.1:8000/")
         cy.get('.icono-nav').click()
         cy.get('#id_username').type("usuario@admin.com")
         cy.get('#id_password').type("contraseña")
